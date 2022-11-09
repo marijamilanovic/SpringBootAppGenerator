fb11d001465994d15a686c5d473c7cb271a4fddd
Maja Dragojlovic <maja98dragojlovic@gmail.com>
2022-11-09 17:44:10 +0100

    [FeatureUpdate] model, service and repository generators

--- PluginDevelopment/PluginDevelopment/src/myplugin/MyPlugin.java
+++ PluginDevelopment/PluginDevelopment/src/myplugin/MyPlugin.java
@@ -29,10 +29,29 @@ public class MyPlugin extends com.nomagic.magicdraw.plugins.Plugin {
 		 * ProjectOptions.xml and take ejb generator options */
 		
 		//for test purpose only:
-		GeneratorOptions ejbOptions = new GeneratorOptions("c:/temp", "ejbclass", "templates", "{0}.java", true, "ejb"); 				
+		GeneratorOptions ejbOptions = new GeneratorOptions("c:/temp/mbrs/src/main/java", "ejbclass", "templates", "{0}.java", true, "ejb"); 				
 		ProjectOptions.getProjectOptions().getGeneratorOptions().put("EJBGenerator", ejbOptions);
-				
 		ejbOptions.setTemplateDir(pluginDir + File.separator + ejbOptions.getTemplateDir()); //apsolutna putanja
+		
+		//Controller
+		GeneratorOptions controllerOptions = new GeneratorOptions("c:/temp/mbrs/src/main/java", "controller", "templates", "{0}Controller.java", true, "uns.ftn.mbrs.controller");
+		ProjectOptions.getProjectOptions().getGeneratorOptions().put("ControllerGenerator", controllerOptions);
+		controllerOptions.setTemplateDir(pluginDir + File.separator + controllerOptions.getTemplateDir());
+		
+		//Service
+		GeneratorOptions serviceOptions = new GeneratorOptions("c:/temp/mbrs/src/main/java", "service", "templates", "{0}Service.java", true, "uns.ftn.mbrs.service");
+		ProjectOptions.getProjectOptions().getGeneratorOptions().put("ServiceGenerator", serviceOptions);
+		serviceOptions.setTemplateDir(pluginDir + File.separator + serviceOptions.getTemplateDir());
+
+		GeneratorOptions serviceImplOptions = new GeneratorOptions("c:/temp/mbrs/src/main/java", "serviceImpl", "templates", "{0}ServiceImpl.java", true, "uns.ftn.mbrs.service.impl");
+		ProjectOptions.getProjectOptions().getGeneratorOptions().put("ServiceImplGenerator", serviceImplOptions);
+		serviceImplOptions.setTemplateDir(pluginDir + File.separator + serviceImplOptions.getTemplateDir());
+		
+		//Repository
+		GeneratorOptions repositoryOptions = new GeneratorOptions("c:/temp/mbrs/src/main/java", "repository", "templates", "{0}Repository.java", true, "uns.ftn.mbrs.repository");
+		ProjectOptions.getProjectOptions().getGeneratorOptions().put("RepoGenerator", repositoryOptions);
+		repositoryOptions.setTemplateDir(pluginDir + File.separator + repositoryOptions.getTemplateDir());
+
 	}
 
 	private NMAction[] getSubmenuActions()
