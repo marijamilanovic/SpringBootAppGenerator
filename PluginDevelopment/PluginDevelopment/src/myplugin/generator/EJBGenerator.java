8e123dc692e29e1541df103789217010570df703
Maja Dragojlovic <maja98dragojlovic@gmail.com>
2022-11-13 23:23:19 +0100

    [Feature] model template

--- PluginDevelopment/PluginDevelopment/src/myplugin/generator/EJBGenerator.java
+++ PluginDevelopment/PluginDevelopment/src/myplugin/generator/EJBGenerator.java
@@ -2,6 +2,7 @@ package myplugin.generator;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +12,9 @@ import javax.swing.JOptionPane;
 import freemarker.template.TemplateException;
 import myplugin.generator.fmmodel.FMClass;
 import myplugin.generator.fmmodel.FMModel;
+import myplugin.generator.fmmodel.FMPersistenceProperty;
+import myplugin.generator.fmmodel.FMProperty;
+import myplugin.generator.fmmodel.FMReferencedProperty;
 import myplugin.generator.options.GeneratorOptions;
 
 /**
@@ -46,9 +50,42 @@ public class EJBGenerator extends BasicGenerator {
 				if (out != null) {
 					context.clear();
 					context.put("class", cl);
-					context.put("properties", cl.getProperties());
+					//context.put("properties", cl.getProperties());
 					context.put("importedPackages", cl.getImportedPackages());
 					context.put("referencedProperties", cl.getReferencedProperties());
+					List<FMProperty> persistentProperties = new ArrayList<FMProperty>();
+					List<FMProperty> properties = new ArrayList<FMProperty>();
+					System.out.println("Properties:" + cl.getProperties().size());
+					for (FMProperty fmProperty : cl.getProperties()) {
+						if (fmProperty instanceof FMPersistenceProperty) {
+							persistentProperties.add(fmProperty);
+						}else {
+							properties.add(fmProperty);
+						}
+					}
+					context.put("persistentProperties", persistentProperties);
+					System.out.println("PROPERTIES");
+					for (FMProperty fmProperty : properties) {
+						System.out.println(fmProperty.getName());
+					}
+					context.put("properties", properties);
+					System.out.println("********REFERENCED************");
+					System.out.println(cl.getReferencedProperties().size());
+					for (FMReferencedProperty fmProperty : cl.getReferencedProperties()) {
+						System.out.println(fmProperty);
+						System.out.println(fmProperty.getType().getClass().getName());
+						System.out.println(fmProperty.getName());
+						System.out.println(fmProperty.getType());
+					}
+					System.out.println("********************");
+					
+					System.out.println("********PERSISTENT************");
+					for (FMProperty fmProperty : persistentProperties) {
+						System.out.println(fmProperty.getName());
+						System.out.println(fmProperty.getType());
+					}
+					System.out.println("********************");
+					
 					getTemplate().process(context, out);
 					out.flush();
 				}
